49de33a342d2cae8cdfae18373a26f33122cf4b8
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2022-03-25 16:03:42 +0100

    Add tailwind & setup build process

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,13 @@
-module.exports = function () {
+module.exports = function (eleventyConfig) {
+	/* 
+		WATCH TARGETS https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
+	*/
+	eleventyConfig.addWatchTarget('./.eleventy.js')
+	eleventyConfig.addWatchTarget('./package.json')
+	eleventyConfig.addWatchTarget('./tailwind.config.js')
+	eleventyConfig.addWatchTarget('./postcss.config.js')
+	eleventyConfig.addWatchTarget('./src/css/')
+
 	return {
 		dir: {
 			input: 'src',
